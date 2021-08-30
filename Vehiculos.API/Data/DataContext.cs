b2fe6a62155e22b6c3c0652ddba07845f835d65b
leidygale <78891427+leidygale@users.noreply.github.com>
2021-08-29 21:37:01 -0500

    Creacion

---
+++ Vehiculos.API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehiculos.API.Data.Entities;
+
+namespace Vehiculos.API.Data
+{
+    public class DataContext : DbContext //La clase hereda de DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)     //ctor y tab tab se crea options es un nombre cualquiera
+        {
+        }
+
+        public DbSet<VehicleType> VehicleTypes { get; set; } //el nombre de la coleccion se le adiciona s
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+                
+        }
+    }
+}
---
+++ Vehiculos.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehiculos.API.Data.Entities
+{
+    public class VehicleType  //Esta es la entidad
+    {
+        public int ID { get; set; } // El Id es el numero autoinrementable
+
+        [Display(Name = "Tipo de vehiculo")]   //Lo que visualiza el usuario
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")] //Controlo la cantidad de caracteres. El 1 muestra la cantidad de caracteres permitido (50)
+        [Required(ErrorMessage = "El campo {0} es obligatorio"] //Significa que el string es un campo obligatorio. En el mensaje el 0 muestra la descripcion tipo de vehiculo
+        public string Description { get; set; }
+    }
+}
